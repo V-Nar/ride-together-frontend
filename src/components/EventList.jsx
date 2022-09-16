797783a6d8c05161c057e1f35c95e8329452c1a4
Aymeric Goube <aymericgoube@hotmail.fr>
2022-09-16 11:55:47 +0200

    connection between front and back end done

---
+++ src/components/EventList.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const EventList = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ride-together.herokuapp.com/api/event")
+      .then((response) => {
+        setEvents(response.data.cityEvents);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!events.length) {
+    return <div className="loading">Loading...</div>;
+  }
+  return (
+    <div>
+      {events.map((event) => (
+        <div>
+          <p>{event.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default EventList;
