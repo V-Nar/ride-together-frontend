bef7361ee8576c693cdcc20f67f415ef0ef03a5a
Aymeric Goube <aymericgoube@hotmail.fr>
2022-09-19 15:11:59 +0200

    added user in authcontext and other modification

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
 // Step one: create a context object to import elsewhere
@@ -5,25 +6,49 @@ const AuthContext = createContext();
 
 // Create the wrapper component
 const AuthContextWrapper = ({ children }) => {
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
-  //   const checkLogin = (token) => {
-  //     if (token) {
-  //       setIsLoggedIn(true);
-  //     } else {
-  //       setIsLoggedIn(false);
-  //     }
-  //   };
+  const checkLogin = (token) => {
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  };
 
   // on initial render, check for existing token
   useEffect(() => {
     const existingToken = localStorage.getItem("AUTH_TOKEN");
     setToken(existingToken);
-    // checkLogin(existingToken);
+    checkLogin(existingToken);
     setIsLoading(false);
   }, []);
+  useEffect(() => {
+    if (token) {
+      axios
+        .get("https://ride-together.herokuapp.com/api/auth/me", {
+          headers: {
+            authorization: "Bearer " + token,
+          },
+          withCredentials: true,
+        })
+        .then((res) => {
+          setUser({
+            image: res.data.user.image,
+            username: res.data.user.username,
+            level: res.data.user.level,
+            id: res.data.user._id,
+          });
+          setIsLoggedIn(true);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [token]);
 
   const updateToken = (token) => {
     localStorage.setItem("AUTH_TOKEN", token);
@@ -38,7 +63,14 @@ const AuthContextWrapper = ({ children }) => {
 
   return (
     <AuthContext.Provider
-      value={{ isLoading, isLoggedIn, token, setToken: updateToken, logout }}
+      value={{
+        isLoading,
+        isLoggedIn,
+        token,
+        user,
+        setToken: updateToken,
+        logout,
+      }}
     >
       {children}
     </AuthContext.Provider>
