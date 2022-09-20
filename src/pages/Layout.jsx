2bbd8913e2054cba6dc7411f3e281b908b34a9ce
V-Nar <103420531+V-Nar@users.noreply.github.com>
2022-09-20 16:46:40 +0200

    Update Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import NavBar from "../components/Navbar.jsx";
+import NavBar from "../components/NavBar.jsx";
 
 const Layout = () => {
   return (
