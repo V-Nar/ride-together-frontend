034be33f870699c3c5b0aa49b228086b69d7b326
V-Nar <valery.narboni@gmail.com>
2022-09-20 15:18:03 +0200

    reverted app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import "./App.css"
-import EventList from "./components/EventList"
-import { Route, Routes } from "react-router-dom"
-import LoginPage from "./pages/LoginPage"
-import Layout from "./pages/Layout"
+import "./App.css";
+import EventList from "./components/EventList";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import Layout from './pages/Layout'
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route element={<Layout />}>
+      <Route element={<Layout />}>
           <Route path="/" element={<EventList />} />
           <Route path="login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Route>
+
       </Routes>
     </div>
   );
