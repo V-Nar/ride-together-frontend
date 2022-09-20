c86898ae1f838e2cc383e0c07181e48631d35cc7
Aymeric Goube <aymericgoube@hotmail.fr>
2022-09-20 16:40:07 +0200

    optimizing form of update user

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import NavBar from '../components/NavBar.jsx'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/Navbar.jsx";
 
 const Layout = () => {
   return (
     <div>
-        <NavBar />
-        <Outlet />
+      <NavBar />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
