30628b0cadd7f28d04a34df14f8d29daf7dbc4eb
V-Nar <valery.narboni@gmail.com>
2022-09-21 18:10:30 +0200

    changed EventList comp to make it reusable, create other components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
-import EventList from "./components/EventList";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import CreateEvent from "./pages/CreateEvent";
 import SignUp from "./pages/SignUp";
 import Event from "./pages/Event";
 import About from "./pages/About";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<EventList />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="signup" element={<SignUp />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="profile" element={<ProfilePage />} />
           <Route path="new" element={<CreateEvent />} />
-          <Route path="event/:id" element={<Event />} />
-          <Route path="my-events" element={<EventList />} />
-          <Route path="my-attendies" element={<EventList />} />
+          <Route path="events/:id" element={<Event />} />
           <Route path="about" element={<About />} />
+          <Route path="*" element={<h2>404 not found</h2>} />
         </Route>
       </Routes>
     </div>
